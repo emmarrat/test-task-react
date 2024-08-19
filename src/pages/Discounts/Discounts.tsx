078ba28f6f812fc27936f6cd166173b8e36e9746
Emir <emmarrat@gmail.com>
2024-08-19 19:59:30 +0600

    feat: updated classes

--- src/pages/Discounts/Discounts.tsx
+++ src/pages/Discounts/Discounts.tsx
@@ -2,8 +2,8 @@ import { DiscountCodeInput, GenerateCodeButton, NoteField, RadioGroup } from '@/
 
 export const Discounts = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
-      <div className="p-6 bg-white rounded-lg shadow-md space-y-6 w-full max-w-md">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-3">
+      <div className="p-6 bg-white rounded-lg shadow-md space-y-6 w-full max-w-md md:max-w-2xl lg:max-w-3xl border md:border md:border-gray-300">
         <RadioGroup />
         <DiscountCodeInput />
         <GenerateCodeButton />
